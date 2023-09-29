718b6c5d8265d7024ce3d3e8b3f759dbc0a0a47b
Vanessa Furquim <vanessafurquim.contato@gmail.com>
2023-09-29 20:02:50 -0300

    Requirements: POST /signUp and POST signIn (authentication routes) - implemented and tested

--- src/controllers/AuthenticationControllers.js
+++ src/controllers/AuthenticationControllers.js
@@ -8,12 +8,12 @@ export async function signUp (request, response) {
     if (password !== confirmPassword) return response.status(422).send( {message: "Password and confirmPassword must match."} )
 
     try {
-        const isEmailRegistered = await db.query(`SELECT email FROM users;`)
-        if (isEmailRegistered) return response.status(409).send( { message: "This email is already registered to an account!" } )
+        const isEmailRegistered = await db.query(`SELECT email FROM users WHERE email = $1;`, [email])
+        if (isEmailRegistered.rowCount !== 0) return response.status(409).send( { message: "This email is already registered to an account!" } )
 
         const hash = bcrypt.hashSync(password, 12)
 
-        await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`, [name, email, password])
+        await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`, [name, email, hash])
         
         response.sendStatus(201)
 
@@ -24,16 +24,16 @@ export async function signIn (request, response) {
     const { email, password } = request.body
 
     try {
-        const isEmailRegistered = await db.query(`SELECT email FROM users;`)
-        if (!isEmailRegistered) return response.status(401).send( { message: "This email address is not registered to any account!" } )
+        const isEmailRegistered = await db.query(`SELECT * FROM users WHERE email = $1;`, [email])
+        if (isEmailRegistered.rowCount === 0) return response.status(401).send( { message: "This email address is not registered to any account!" } )
 
-        const isPasswordCorrect = bcrypt.compareSync(password, userInformation.password)
+        const isPasswordCorrect = bcrypt.compareSync(password, isEmailRegistered.rows[0].password)
         if (!isPasswordCorrect) return response.status(401).send( { message: "Invalid password!" } )
 
         const token = uuid()
-        await db.collection("sessions").insertOne( { token, userId: user._id } )
+        await db.query(`INSERT INTO "authenticationSessions" (token, "userId") VALUES ($1, $2)`, [token, isEmailRegistered.rows[0].id])
 
         response.status(200).send( { token: `${token}` } )
-
+    
     } catch (error) { response.status(500).send(error.message) }
 }
