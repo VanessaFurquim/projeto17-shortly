284a72e97f5feb923d087c8506de51c692e8898d
Vanessa Furquim <vanessafurquim.contato@gmail.com>
2023-09-30 03:39:50 -0300

    Requirements: POST /urls/shorten - body of response adjusted to conform to requirement; GET /urls/:id - implemented and tested.

--- src/controllers/urlsControllers.js
+++ src/controllers/urlsControllers.js
@@ -1,16 +1,34 @@
 import db from "../database/databaseConfig.js"
 import { nanoid } from 'nanoid/non-secure'
 
-export async function shortenUrl (request, response) {
+export async function shorteningUrl (request, response) {
     const { url } = request.body
     const userId = response.locals.rows[0].id
 
     const shortUrl = nanoid()
 
+    let insertRequestData
+
     try {
-        const x = await db.query(`INSERT INTO urls ("originalUrl", "shortUrl", "userId") VALUES ($1, $2, $3)`, [url, shortUrl, userId])
+        insertRequestData = await db.query(`INSERT INTO urls ("originalUrl", "shortUrl", "userId") VALUES ($1, $2, $3) RETURNING id`, [url, shortUrl, userId])
 
     } catch (error) { response.status(500).send(error.message) }
 
-    response.status(201).send(shortUrl)
+    response.status(201).send( { id: (insertRequestData.rows[0].id), shortUrl } )
+}
+
+export async function getUrlById (request, response) {
+    const { id } = request.params
+
+    try {
+        const isShortUrlExistent = await db.query(`SELECT "originalUrl", "shortUrl" FROM urls WHERE id = $1`, [id])
+        if (isShortUrlExistent.rowCount === 0) return response.status(404).send( {message: "The shortURL you are trying to access does not exist."})
+
+        response.status(200).send( {
+            id,
+            shortUrl: isShortUrlExistent.rows[0].shortUrl,
+            url: isShortUrlExistent.rows[0].originalUrl
+        } )
+
+    } catch (error) { response.status(500).send(error.message) }
 }
